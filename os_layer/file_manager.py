7bb02e10f36f773f85177b64f12ead2c6119efe2
vishwasyk2005 <vishwasyk2005@gmail.com>
2026-02-26 13:20:11 +0530

    Update file_manager.py

--- os_layer/file_manager.py
+++ os_layer/file_manager.py
@@ -1,1 +1,13 @@
+def read_binary(filepath, chunk_size=4096):
+    with open(filepath, "rb") as f:
+        while True:
+            chunk = f.read(chunk_size)
+            if not chunk:
+                break
+            yield chunk
+
+
+def write_binary(filepath, data):
+    with open(filepath, "wb") as f:
+        f.write(data)
 
